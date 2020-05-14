5a7c45f9a92dcfa8f91f958f1dcc363ff381b060
ngarci20 <ngarci@uoc.edu>
2020-05-14 12:14:41 +0200

    First commit

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-function readyB() {
-document.getElementById("bundle1").style.display = "block";
-document.getElementById("bundle2").style.display = "none";
-}
+import style from './style';
 
-function readyC() {
-document.getElementById("compile2").style.display = "none";
-document.getElementById("compile1").style.display = "block";
-
-}
-
-function Bundle() {
-    document.getElementById("bundle1").style.display = "none";
-    document.getElementById("bundle2").style.display = "block";
-}
-
-function Compile() {
-    document.getElementById("compile1").style.display = "none";
-    document.getElementById("compile2").style.display = "block";
-}
+style();
---
+++ style.js
@@ -0,0 +1,4 @@
+import classes from './style.css';
+export default () => {
+  console.log(classes.animar);
+};
